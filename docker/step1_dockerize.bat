8a0760b2f9cd89c5253577cc8c3eb5ba4bc08b3e
Maxwolf <bigmaxwolf@gmail.com>
2023-04-13 20:41:01 -0700

    Add native docker support through IDE instead of manually

--- docker/step1_dockerize.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Stopping and removing existing container...
-docker container stop vintagehive
-docker container rm vintagehive
-
-echo Building docker image...
-docker build -t foxcouncil/vintagehive .
