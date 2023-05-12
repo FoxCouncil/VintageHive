fa418f54882b5fd2b72ae756bde9a3134305ea61
Maxwolf <bigmaxwolf@gmail.com>
2023-05-12 12:24:09 -0700

    Got news articles viewing correctly, still some formatting errors with text flow.

--- Proxy/Telnet/Commands/News/TelnetNewsArticleView.cs
+++ Proxy/Telnet/Commands/News/TelnetNewsArticleView.cs
@@ -1,4 +1,6 @@
-﻿namespace VintageHive.Proxy.Telnet.Commands.News;
+﻿using SmartReader;
+
+namespace VintageHive.Proxy.Telnet.Commands.News;
 
 public class TelnetNewsArticleView : ITelnetWindow
 {
@@ -16,55 +18,103 @@ public class TelnetNewsArticleView : ITelnetWindow
 
     private string _text;
     private TelnetSession _session;
+    private Headlines _headline;
     private bool _shouldRemoveNextCommand;
 
-    private const int PageSize = 500;
+    private const int LinesPerPage = 80;
     private int totalPages = 1;
     private int currentPage = 1;
 
-    private string wrappedArticleText;
+    private string articleText;
+    private string[] words = null;
+    private System.Timers.Timer _startupDelay = new System.Timers.Timer();
 
     public void OnAdd(TelnetSession session, object args = null)
     {
         _session = session;
 
-        // Convert our headline ID into a full article.
-        var headline = args as Headlines;
-        var article = NewsUtils.GetGoogleNewsArticle(headline.Id).Result;
-        var rawArticleText = article.TextContent.ReplaceLineEndings("\r\n");
-        wrappedArticleText = article.Title.WordWrapText(_session.TermWidth, _session.TermHeight) + "\r\n";
-        wrappedArticleText += rawArticleText.WordWrapText(_session.TermWidth, _session.TermHeight);
+        // Object containing title, id, and other data passed to us from headline viewer.
+        _headline = args as Headlines;
 
-        // Figure out how many pages this text will take to display.
-        totalPages = (int)Math.Ceiling((double)wrappedArticleText.Length / PageSize);
-        currentPage = 1;
+        _text = "Loading article...\r\n";
 
-        UpdateArticleText();
+        _startupDelay.AutoReset = false;
+        _startupDelay.Elapsed += Startup_Timer;
+        _startupDelay.Interval = 1;
+        _startupDelay.Enabled = true;
+        _startupDelay.Start();
+    }
+
+    private async void Startup_Timer(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        // Never run the timer again.
+        _startupDelay.Stop();
+        _startupDelay.Enabled = false;
+
+        // Create a cancellation token source with a timeout of 5 seconds
+        var timeout = 1000;
+        var cancellationToken = new CancellationToken();
+
+        // Start the blocking task
+        Task<Article> task = DownloadArticle(_headline.Id);
+
+        if (await Task.WhenAny(task, Task.Delay(timeout, cancellationToken)) == task)
+        {
+            // Task completed within timeout.
+            // Consider that the task may have faulted or been canceled.
+            // We re-await the task so that any exceptions/cancellation is rethrown.
+            await task;
+
+            var article = task.Result;
+            var rawArticleText = article.TextContent.ReplaceLineEndings("\r\n");
+            articleText = article.Title.ReplaceLineEndings(string.Empty) + "\r\n";
+            articleText += rawArticleText;
+
+            // Figure out how many pages this text will take to display.
+            words = articleText.Split(' ');
+            totalPages = (int)Math.Ceiling((double)words.Length / LinesPerPage);
+            currentPage = 1;
+
+            UpdateArticleText();
+        }
+        else
+        {
+            // timeout/cancellation logic
+            _text = "Failed to load article, type [q]uit to return to headline view.\r\n";
+            Log.WriteLine(Log.LEVEL_ERROR, nameof(TelnetNewsArticleView), $"Client {_session.Client.RemoteIP} timeout occurred getting news article {_headline.Id}. Task cancelled.", _session.Client.TraceId.ToString());
+        }
+
+        // Since this happens on another thread and at a random time we have to force a tick to update screen!
+        await _session.TickWindows();
+    }
+
+    private static async Task<Article> DownloadArticle(string id)
+    {
+        return await NewsUtils.GetGoogleNewsArticle(id);
     }
 
     private void UpdateArticleText()
     {
-        int startIndex = (currentPage - 1) * PageSize;
-        int endIndex = Math.Min(startIndex + PageSize, wrappedArticleText.Length);
-        string pageText = wrappedArticleText.Substring(startIndex, endIndex - startIndex);
+        // Skip if no work to do!
+        if (string.IsNullOrEmpty(articleText))
+        {
+            return;
+        }
+
+        int startIndex = (currentPage - 1) * LinesPerPage;
+        int endIndex = Math.Min(startIndex + LinesPerPage, words.Length);
 
         // Build up article to display, keeping pagenation in mind.
         var result = new StringBuilder();
         result.Append($"News Article Viewer - Page {currentPage}/{totalPages}\r\n");
         result.Append("[N]ext page  [P]revious page  [Q]uit\r\n\r\n");
-        
-        if (currentPage >= totalPages)
+
+        for (int i = startIndex; i < endIndex; i++)
         {
-            // Last page will have proper return on it.
-            result.Append(pageText);
-        }
-        else
-        {
-            // Cannot be guranteed of this otherwise!
-            result.Append($"{pageText}\r\n");
+            result.Append($"{words[i]} ");
         }
 
-        _text = result.ToString();
+        _text = result.ToString().WordWrapText(_session.TermWidth, _session.TermHeight);
     }
 
     public void Destroy() { }
